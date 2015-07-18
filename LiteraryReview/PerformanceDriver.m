4429ad881f644f0c4a4823adeacb60fb95471f3f
tghaddar <tghaddar92@gmail.com>
2015-07-17 23:18:19 -0500

    Working Performance Driver and Model Code

--- LiteraryReview/PerformanceDriver.m
+++ LiteraryReview/PerformanceDriver.m
@@ -1,11 +1,11 @@
-function [] = PerformanceDriver(M_L)
+function [RealEff] = PerformanceDriver(M_L)
 %A code that optimizes the performance model for each processor layout
 %INPUT: 
 %M_L = A latency factor that dictates whether the model is high or low
 %overhead.
 %P: A 15x3 matrix that stores the breakdown of processors in x, y,
 %          and z for each total processor count.
-P = [1,1,1;2,2,2;8,4,2;16,16,2;32,16,2;32,32,2;64,32,2;64,64,2;128,64,2;128,128,2;256,128,0;256,256,2;512,256,2;512,512,2;1024,512,2];
+P = [1,1,1;2,2,2;8,4,2;16,16,2;32,16,2;32,32,2;64,32,2;64,64,2;128,64,2;128,128,2;256,128,2;256,256,2;512,256,2;512,512,2;1024,512,2];
 %N: A 15x3 matrix that stores the breakdown of cells in x,y, and z.
 N = [16,16,16;32,32,32;64,64,64;128,128,128;128,128,256;256,128,256;256,256,256;256,256,512;512,256,512;512,512,512;512,512,1024;1024,512,1024;1024,1024,1024;1024,1024,2048;2048,1024,2048];
 %The processor array, stores the breakdown of processors in x, y, and z.
@@ -21,7 +21,9 @@ T_latency = 4114.74;
 N_bytes = [0 1280 960 640 480 480 480 320 320 320 160 160 160 80 80];
 %The parallel efficiency for a serial case is obviously 1.
 RealEff(1) = 1.0;
+P_total(1) = 1;
 for i = 2:size(P,1)
+    P_total(i) = P(i,1)*P(i,2)*P(i,3);
     %This is the iterative processess to determine the optimized
     %aggregation factors for the current cell and processor layouts.
     %In each direction, the start point is A_u = 1, and the end point is
@@ -34,15 +36,24 @@ for i = 2:size(P,1)
     for a = Ax_start:Ax_end
        for b = Ay_start:Ay_end
            for c = Az_start:Az_end
-               A = [a b c];
-               eff = PerformanceModel(M_L,T_latency,T_byte,N_bytes(i),T_grind,P,A,N);
+               A = [a b c 10 1];
+               N_Current = N(i,:);
+               P_Current = P(i,:);
+               eff = PerformanceModel(M_L,T_latency,T_byte,N_bytes(i),T_grind,P_Current,A,N_Current);
                if (eff >= opt_eff)
-                  opt_eff = eff; 
+                  opt_eff = eff;
+                  RealEff(i) = opt_eff;
                end
            end
        end
     end
 end
+
+plot(P_total,RealEff);
+title('Parallel Efficiency of PDTs Performance Model');
+xlabel('Processors');
+ylabel('Parallel Efficiency');
+ylim([0,1]);
 
 end
 
--- LiteraryReview/PerformanceModel.m
+++ LiteraryReview/PerformanceModel.m
@@ -11,7 +11,7 @@ function [eff] = PerformanceModel(M_L,T_latency,T_byte,N_bytes,T_grind,P,A,N)
 %   order: A_x,A_y,A_z,A_m,A_g
 %N: The number of cells in each direction in a vector stored in the
 %   following order N_x, N_y, N_z
-%P is the number of processors 
+%P: The P_x, P_y, P_z processors
 
 %The time to communicate between processors
 T_comm = M_L*T_latency + T_byte*N_bytes;
@@ -22,14 +22,14 @@ for i = 1:length(A)
 end
 %The time to communicate a task
 T_task = aggregation_product*T_grind;
-
 %The aggregation in Z
 A_z = A(3);
 %The number of cellsets
-N_z = P_z*A_z*4;
-N_k = N_z/(P_z*A_z);
+N_z = N(3);
+N_k = N_z/(P(3)*A_z);
+M = 10;
 %The parallel efficiency
-eff = ;
+eff = ((1 + (P(1)+ P(2) - 4 + N_k*(P(3)-2))/(8*M*N_k/(A(4)*A(5)))) * (1+T_comm/T_task) )^(-1);
 
 
 end
