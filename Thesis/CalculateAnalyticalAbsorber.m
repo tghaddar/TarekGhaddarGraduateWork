438e48a8449c99ba873145dad2a083c24cc3b789
tghaddar <tghaddar@gmail.com>
2016-02-15 19:03:57 -0600

    Adding some files and workspaces.

---
+++ Thesis/CalculateAnalyticalAbsorber.m
@@ -0,0 +1,28 @@
+function [flux] = CalculateAnalyticalAbsorber( num_angles_polar,num_angles_az, analyticalX )
+%Calculates the analytical solution using quadrature.
+
+[nodes,weights] = lgwt(2*num_angles_polar,-1,1);
+psi_incident = 7/(2*pi);
+
+weight_az = 2*pi/(4*num_angles_az);
+weight_az
+
+flux = zeros(length(analyticalX),1);
+
+for i = 1:length(analyticalX)
+   x_val = analyticalX(i);
+   flux_val = 0;
+   for j = 1:length(nodes)
+      mu_val = abs(nodes(j));
+      polar_weight = weights(j);
+      flux_val = flux_val + psi_incident*polar_weight*weight_az*exp(-5*x_val/mu_val);
+   end
+   
+   flux(i) = flux_val;
+    
+end
+
+
+
+end
+
--- Thesis/PlotPureAbsorber.m
+++ Thesis/PlotPureAbsorber.m
@@ -1,17 +1,25 @@
-function [L2_error] = PlotPureAbsorber( PureAbsorber )
+function [L2_error,flux] = PlotPureAbsorber( PureAbsorber1)
 hold off;
-analyticalX = PureAbsorber(:,1);
-s = 3.5*expint(2,sym(5*analyticalX));
-analyticalY = vpa(s,10);
+analyticalX = PureAbsorber1(:,1);
+% s = 3.5*expint(2,sym(5*analyticalX));
+% analyticalY = vpa(s,10);
 
-error = abs(analyticalY-PureAbsorber(:,2));
-L2_error = norm(error);
+flux = CalculateAnalyticalAbsorber(2,1,analyticalX);
+
+error = abs(flux-PureAbsorber1(:,2));
+L2_error = norm(error)/norm(flux);
 
 hold on;
 
-plot(analyticalX,analyticalY);
-plot(PureAbsorber(:,1),PureAbsorber(:,2),'--');
-legend('Analytical','PDT');
+plot(analyticalX,flux);
+plot(PureAbsorber1(:,1),PureAbsorber1(:,2),'--');
+% plot(PureAbsorber2(:,1),PureAbsorber2(:,2),'--');
+% plot(PureAbsorber3(:,1),PureAbsorber3(:,2),'--');
+% plot(PureAbsorber4(:,1),PureAbsorber4(:,2),'--');
+% plot(PureAbsorber5(:,1),PureAbsorber5(:,2),'--');
+
+legend('Analytical','PDT_{pglc}');
+title('A Pure Absorber 1D Slab');
 hold off;
 
 end
