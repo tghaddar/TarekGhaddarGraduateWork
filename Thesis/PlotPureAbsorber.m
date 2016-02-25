64fad3a10470b07023065046afc0a1d3ba826aab
tghaddar <tghaddar@gmail.com>
2016-02-25 16:51:29 -0600

    More results added, benchmark solution stuff complete.

--- Thesis/PlotPureAbsorber.m
+++ Thesis/PlotPureAbsorber.m
@@ -1,5 +1,6 @@
-function [L2_error,flux] = PlotPureAbsorber( PureAbsorber1)
+function [L2_error] = PlotPureAbsorber( PureAbsorber1,PureAbsorber2,PureAbsorber3,PureAbsorber4)
 hold off;
+close all; clc;
 analyticalX = PureAbsorber1(:,1);
 s = 3.5*expint(2,sym(5*analyticalX));
 flux = vpa(s,10);
@@ -7,19 +8,21 @@ flux = vpa(s,10);
 %flux = CalculateAnalyticalAbsorber(1,20,analyticalX);
 
 error = abs(flux-PureAbsorber1(:,2));
-L2_error = norm(error)/norm(flux);
+L2_error = vpa(norm(error)/norm(flux),10);
 
 hold on;
 
 plot(analyticalX,flux);
 plot(PureAbsorber1(:,1),PureAbsorber1(:,2),'--');
-%plot(PureAbsorber2(:,1),PureAbsorber2(:,2),'--');
+% plot(PureAbsorber2(:,1),PureAbsorber2(:,2),'--');
 % plot(PureAbsorber3(:,1),PureAbsorber3(:,2),'--');
 % plot(PureAbsorber4(:,1),PureAbsorber4(:,2),'--');
-% plot(PureAbsorber5(:,1),PureAbsorber5(:,2),'--');
 
-legend('Analytical','PDT_{pglc}');
-title('A Pure Absorber 1D Slab');
+%legend('Analytical','PDT_{pglc70}','PDT_{pglc10}','PDT_{pglc5}','PDT_{pglc1}');
+legend('Analytical','PDT_{pglc70}');
+title('A Pure Absorber 1D Slab with Different Angular Refinements');
+xlabel('Distance (cm)');
+ylabel('Flux (neutrons/cm^{2}-s'); 
 hold off;
 
 end
--- Thesis/PlotPureScatterer.m
+++ Thesis/PlotPureScatterer.m
@@ -2,7 +2,15 @@ function [L2_error] = PlotPureScatterer( PureScatterer )
 hold off;
 close all; clc;
 analyticalX = PureScatterer(:,1);
-analyticalY = -3*analyticalX + 5;
+
+XS = 100;
+D = 1/(3*XS);
+incident = 7;
+A = -incident/(1+4*D);
+B = incident + 2*D*A;
+
+
+analyticalY = A*analyticalX + B;
 
 error = abs(analyticalY-PureScatterer(:,2));
 L2_error = norm(error)/norm(analyticalY);
@@ -13,6 +21,9 @@ plot(analyticalX,analyticalY);
 plot(PureScatterer(:,1),PureScatterer(:,2),'--');
 legend('Analytical','PDT');
 title('A 1D Pure Scatterer');
+xlabel('Distance (cm)');
+ylabel('Flux (neutrons/cm^{2}-s)');
+
 hold off;
 
 end
