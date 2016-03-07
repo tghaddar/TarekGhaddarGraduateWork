07d06c73adf3c3d7ca8275ab4e56e8d4040ac4ad
tghaddar <tghaddar@gmail.com>
2016-03-07 13:28:15 -0600

    Complete draft of thesis.

--- Defense/redistribute.cc
+++ Defense/redistribute.cc
@@ -1,17 +1,14 @@
-stapl::array_view num_tri_view
-stapl::array_view offset_view
-//offset_view stores partial sum of num_tri_view
-stapl::partial_sum(num_tri_view) 
+//stores number of triangles for each row/col 
+num_tri_view
+//stores the partial sum of num_tri_view
+offset_view
 //We now have a cumulative distribution stored in offset_view
 for (i = 1:X.size()-1)
 {
-   vector <double> pt1 = [CutLines(i-1), offset_view(i-1)]
-   vector <double> pt2 = [CutLines(i), offset_view(i)]
-   ideal_value = i*(N_tot/num_subsets_X);
+   pt1 = [X(i-1), offset_view(i-1)]
+   pt2 = [x(i), offset_view(i)]
+   ideal_num_triangles = i*(N_tot/num_subsets_X);
    x_val = X-intersect(pt1,pt2,ideal_value);
-   if ((x_val > x_cuts[i-1] && x_val< x_cuts[i]) 
-     || equal(x_val,x_cuts[i]) || equal(x_val,x_cuts[i-1])
-   {
-     X[i] = x_val; 
-   }
+   //The cut line in question has been redistributed.  
+   X[i] = x_val; 
 }
