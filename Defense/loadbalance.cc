bd76f15dce553f4091ecf6bcad3336a6be3dc76a
tghaddar <tghaddar@gmail.com>
2016-03-06 20:23:45 -0600

    Defense presentation files. Still working on it.

---
+++ Defense/loadbalance.cc
@@ -0,0 +1,17 @@
+//I,J subsets specified by user
+//Check if all subsets meet the tolerance
+while (f < tol_subset)
+{
+  //Mesh all subsets
+  else
+  {
+    if (f_I > tol_column)
+    {
+      Redistribute(X); 
+    }
+    if (f_J > tol_row)
+    {
+      Redistribute(Y);
+    }
+  }
+}
