957d5ac4f115ff29a155766d7045beaf538e2fcf
tghaddar <tghaddar92@gmail.com>
2016-05-19 16:19:35 +0300

    Simply copying the thesis files into a new folder. Removals and other changes are coming.

---
+++ MandC2017/loadbalance.cc
@@ -0,0 +1,15 @@
+//I,J subsets specified by user
+//Check if all subsets meet the tolerance
+while (f > tol_subset)
+{
+  //Mesh all subsets
+  if (f_I > tol_column)
+  {
+    Redistribute(X); 
+  }
+  if (f_J > tol_row)
+  {
+    Redistribute(Y);
+  }
+  
+}
