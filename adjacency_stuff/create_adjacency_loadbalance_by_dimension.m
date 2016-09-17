da9ae1f362cfc5a95d9bd72846c6e70b2f4b47d2
Jean Ragusa <jean.ragusa@tamu.edu>
2016-09-17 02:17:02 -0500

    testing load by dimension stuff

---
+++ adjacency_stuff/create_adjacency_loadbalance_by_dimension.m
@@ -0,0 +1,109 @@
+close all; clear all; clc;
+
+do_random=true;
+
+% number of cut lines
+n_cutx = 5;
+n_cuty = 3;
+
+% number of subsets per dimension
+nx = n_cutx + 1;
+ny = n_cuty + 1;
+
+if do_random
+    % cut lines in x
+    X = [0 sort(rand(1,n_cutx)) 1];
+    % cut lines in y, per column
+    Y=zeros(nx,n_cuty+2);
+    for i=1:nx
+        Y(i,:) = [0 sort(rand(1,n_cuty)) 1];
+    end
+else
+    X = linspace(0,1,n_cutx+2);
+    tmp = linspace(0,1,n_cuty+2);
+    for i=1:nx
+        Y(i,:) = tmp;
+    end
+end
+
+% adjacency matrix
+n=nx*ny;
+A = zeros(n,n);
+
+% loop over columns 
+for i=1:nx
+    % beginning and end rows in A for this colum
+    row_beg = (i-1)*ny+1;
+    row_end = (i  )*ny  ;
+    % up/down neighbors
+    for row=row_beg:row_end
+        % top neighbor
+        if row~=row_end, A(row,row+1)=1; end
+        % not need for bottom neighbors, we use transport at the end
+        % % bottom neighbor
+        % if row~=row_beg, A(row,row-1)=1; end
+    end
+    
+    % right neighbors, not needed for last column
+    if i<nx 
+        % cut lines of the current column
+        cut_lines_curr = Y(i,:);
+        % cut lines of the right neighbor column
+        cut_lines_neig = Y(i+1,:);
+        for j=1:ny
+            % current row in A matrix
+            current_row = (i-1)*ny+j;
+            % cut lines of interest
+            Y1=cut_lines_curr(j);
+            Y2=cut_lines_curr(j+1);
+            ind = find( (cut_lines_neig>Y1-eps) & (cut_lines_neig<Y2+eps) );
+            % TODO: need fuzzy logic for the vase when vertices are the same
+            % ind is the ID's of the cut lines of the neighbors
+            if ~isempty(ind)
+                connecting_subsets = [ind(1)-1 ind];
+                if abs(Y1 - cut_lines_neig(ind(1)))< 1.5*eps
+                    % remove the first subset
+                    connecting_subsets(1) = [];
+                end
+                if abs(Y2 - cut_lines_neig(ind(end)))< 1.5*eps
+                    % remove the last subset
+                    connecting_subsets(end) = [];
+                end
+                % i*ny: to skip all ofthe subset in all columns from 1 to i included
+                A(current_row,i*ny+connecting_subsets)=1;
+            end % ind is not empty
+        end % we are not yet at the last column
+    end % we are not yet at the last column
+    
+end % loop of columns
+
+% symmetric portion
+A = A + A';
+figure(1)
+spy(A)
+
+% plot the graph
+figure(2)
+Coordinates = zeros(n,2);
+k=0;
+for i=1:nx
+    xval = (X(i)+X(i+1))/2;
+    for j=1:ny
+        yval = (Y(i,j)+Y(i,j+1))/2;
+        k=k+1;
+        Coordinates(k,:)=[xval yval];
+    end
+end
+gplot(A,Coordinates,'r-*');
+hold on
+% superimpose the subset mesh 
+for i=1:nx+1
+    line([X(i),X(i)],[0,1])
+    if i<=nx
+        for j=1:ny+1
+            line([X(i),X(i+1)],[Y(i,j),Y(i,j)])
+        end
+    end
+end
+    
+    
