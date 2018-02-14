4fc12c592c1a1aa08577c1705f7927b9a7ae0da2
tghaddar <tghaddar92@gmail.com>
2018-02-14 15:02:23 -0600

    Adding python code for sweeps.

---
+++ tarek_sweep_code/build_adjacency_matrix.py
@@ -0,0 +1,58 @@
+#This builds the adjacency matrix for all subsets. ycuts are stored by column. 
+def build_adjacency(global_bounds, xcuts, ycuts,i_val,j_val):
+  
+  #The number of subsets in our domain.
+  num_subsets = len(global_bounds)
+  
+  adjacency_matrix = []
+  
+  for s in range(0, num_subsets):
+    #The neighbors of this subset.
+    neighbors = []
+    #The bounds of this subset.
+    xmin = global_bounds[s][0]
+    xmax = global_bounds[s][1]
+    ymin = global_bounds[s][2]
+    ymax = global_bounds[s][3]
+    
+    #The number of interior ycuts in each column
+    n_y = len(ycuts[0]) - 2
+    
+    #The number of columns in our domain.
+    numcol = len(xcuts) - 1
+    #The number of rows in our domain.
+    numrow = n_y + 1
+    
+    #We are only adding right and left neighborsif our domain is more than one subset wide
+    if (numcol != 1):
+      
+      #If we're in any column but the last one, we look to our right for potential neighbors.
+      if (i_val < numcol - 1):
+        #The ycut lines in the column to the right.
+        right_column_y_cuts = ycuts[i_val+1]
+        #Looping over all cuts but the top one.
+        for j in range (0, n_y+1):
+          #Grabbing the enclosure of the potential neighboring subset.
+          cut = right_column_y_cuts[j]
+          next_cut = right_column_y_cuts[j+1]
+          if ( (next_cut == ymin) or (cut == ymax) ):
+            continue
+          elif (ymin < next_cut and ymax > cut):
+            neighbors.append((i_val+1)*numrow + j)
+      #If we're in any column but the first column, we look to our left neighbor.
+      if (i_val > 0):
+        #The ycut lines in the column to the left.
+        left_column_y_cuts = ycuts[i_val - 1]
+        for j in range(0, n_y + 1):
+          cut = left_column_y_cuts[j]
+          next_cut = left_column_y_cuts[j+1]
+          if ( (next_cut == ymin) or (cut == ymax) ):
+            continue;
+          if ( ymin < next_cut and ymax > cut ):
+            neighbors.append((i_val-1)*numrow + j )
+    
+    adjacency_matrix.append(neighbors)        
+    
+  
+  return adjacency_matrix
+
---
+++ tarek_sweep_code/driver.py
@@ -0,0 +1,2 @@
+#importing the adjacency building capabilities.
+from build_adjacency_matrix import build_adjacency
