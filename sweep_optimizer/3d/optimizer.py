e8e8b4bfdd6d21530ccb1dcddcf10847018edb67
Tarek <tghaddar@tamu.edu>
2018-09-27 18:46:24 -0500

    Beginning the constrained optimization process.
    Still need to figure out how to bound the values of the cut lines.

--- sweep_optimizer/3d/optimizer.py
+++ sweep_optimizer/3d/optimizer.py
@@ -16,6 +16,7 @@ import sweep_solver
 import networkx as nx
 import warnings
 from scipy.optimize import minimize
+from scipy.optimize import bounds
 warnings.filterwarnings("ignore")
 
 plt.close("all")
@@ -89,24 +90,38 @@ def plot_subset_boundaries(global_3d_subset_boundaries,num_subsets):
   
   plt.savefig("subset_plot.pdf")
 
+def con(t):
+  
+  return all(i > 0.0 for i in t) and all(i < 10.0 for i in t)
+
 def time_solver(param):
-  x_cuts,y_cuts,z_cuts = param
+  
+  x_cuts_i = param[0:num_plane*N_x]
+  y_cuts_i = param[num_plane*N_x:num_plane*N_x + num_plane*num_col*N_y]
+  z_cuts_i = param[num_plane*N_x + num_plane*num_col*N_y:]
   
   #Adding global z boundaries.
-  z_cuts = [global_z_min] + z_cuts + [global_z_max]
+  z_cuts = [global_z_min] + z_cuts_i + [global_z_max]
   
   #Adding x global boundaries
-  for i in range(0,len(x_cuts)):
-    #x_cuts for this plane
-    p_x_cuts = x_cuts[i]
+  x_cuts = []
+  for p in range(0,num_plane):
+    p_x_cuts = x_cuts_i[p*N_z:N_z*(p+1)]
     p_x_cuts = [global_x_min] + p_x_cuts + [global_x_max]
-    x_cuts[i] = p_x_cuts
+    x_cuts.append(p_x_cuts)
   
-  for j in range(0,len(y_cuts)):
-    for jp in range(0,len(y_cuts[j])):
-      p_y_cuts = y_cuts[j][jp]
-      p_y_cuts = [global_y_min] + p_y_cuts + [global_y_max]
-      y_cuts[j][jp] = p_y_cuts
+  y_cuts = []
+  for p in range(0,num_plane):
+    p_y_cuts = y_cuts_i[p*num_col*N_y:(p+1)*num_col*N_y]
+    plane_cuts = []
+    for c in range(0,num_col):
+      pc_y_cuts = p_y_cuts[c*N_y:(c+1):N_y]
+      plane_cuts.append([global_y_min] + pc_y_cuts + [global_y_max])
+      
+    y_cuts.append(plane_cuts)
+      
+      
+    
 
   #Building the global subset boundaries.
   global_3d_subset_boundaries = b3a.build_3d_global_subset_boundaries(N_x,N_y,N_z,x_cuts,y_cuts,z_cuts)
@@ -141,17 +156,31 @@ def time_solver(param):
       p_y_cuts.pop()
       y_cuts[j][jp] = p_y_cuts
       
-  param = (x_cuts,y_cuts,z_cuts)
+   #Flattening out the cuts again.
+  b = [item for sublist in x_cuts for item in sublist]
+  c = [item for sublist in y_cuts for item in sublist]
+  c = [item for sublist in c for item in sublist]
+  param = b+c+z_cuts
+  
   return time
 
 #Assuming the order of cutting is z,x,y.
 z_cuts = [5]
 #X_cuts per plane.
 x_cuts = [[3],[5]]
+b = [item for sublist in x_cuts for item in sublist]
 #y_cuts per column per plane.
 y_cuts = [[[3],[5]], [[4],[7]]]
+c = [item for sublist in y_cuts for item in sublist]
+c = [item for sublist in c for item in sublist]
+#param0 = np.array([x_cuts,y_cuts,z_cuts])
+param0 = b + c + z_cuts
 
-param = (x_cuts,y_cuts,z_cuts)
+print(con(param0))
+#time = time_solver(param0)
+cons = {'type':'ineq', 'fun':con}
 
-time = time_solver(param)
+sol = minimize(time_solver,param0,constraints=cons)
+
+#print(minimize(time_solver,param).x)
 
