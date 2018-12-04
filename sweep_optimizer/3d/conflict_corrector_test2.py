78618ea00d30c110a7d47ee7b66bb21fde4e0c6d
Tarek <tghaddar@tamu.edu>
2018-12-04 11:02:19 -0600

    Fixing a graphics issue in conflict corrector test 2.

--- sweep_optimizer/3d/conflict_corrector_test2.py
+++ sweep_optimizer/3d/conflict_corrector_test2.py
@@ -9,10 +9,11 @@ A different method of correcting conflicts with 4 paths.
 import networkx as nx
 import matplotlib.pyplot as plt
 import warnings
-warnings.filterwarnings("ignore", category=DeprecationWarning)
 from sweep_solver import add_conflict_weights
 from sweep_solver import get_heaviest_path
 from sweep_solver import get_DOG
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
 plt.close("all")
 
 G = nx.DiGraph()
@@ -29,16 +30,19 @@ G.add_edge(0,1,weight = 3)
 G.add_edge(1,2,weight = 10)
 G.add_edge(2,3,weight = 2)
 G.add_edge(3,4,weight = 8)
-#plt.figure("Graph 1")
-#nx.draw(G,nx.shell_layout(G),with_labels = True)
+plt.figure("Graph 1 Pre Conflict")
+edge_labels_1 = nx.get_edge_attributes(G,'weight')
+nx.draw(G,nx.shell_layout(G),with_labels = True)
+nx.draw_networkx_edge_labels(G,nx.shell_layout(G),edge_labels=edge_labels_1)
 
 G2.add_edge(4,3,weight = 1)
 G2.add_edge(3,2,weight = 8)
 G2.add_edge(2,1,weight = 2)
 G2.add_edge(1,0,weight = 10)
-
-#plt.figure("Graph 2")
-#nx.draw(G2,nx.shell_layout(G2),with_labels = True)
+edge_labels_2 = nx.get_edge_attributes(G2,'weight')
+plt.figure("Graph 2 Pre Conflict")
+nx.draw(G2,nx.shell_layout(G2),with_labels = True)
+nx.draw_networkx_edge_labels(G2,nx.shell_layout(G2),edge_labels=edge_labels_2)
 
 path1 = nx.all_simple_paths(G,0,4)
 path2 = nx.all_simple_paths(G2,4,0)
