53be26bc30a4276473bde485e8d9c50370adc4c0
Tarek <tghaddar@tamu.edu>
2019-01-30 16:22:29 -0600

    Adding some debug plotting.

--- sweep_optimizer/3d/regular_partition_ragusa_test.py
+++ sweep_optimizer/3d/regular_partition_ragusa_test.py
@@ -4,6 +4,7 @@ from build_adjacency_matrix import build_graphs
 from sweep_solver import make_edges_universal
 from sweep_solver import add_conflict_weights
 import matplotlib.pyplot as plt
+
 plt.close("all")
 
 numrow = 4
@@ -19,20 +20,29 @@ for g in range(0,num_graphs):
   graph = graphs[g]
   graph.add_weighted_edges_from((u,v,1) for u,v in graph.edges())
 
-G = graphs[0]
-plt.figure("Test for adding uniform edge weights")
-edge_labels_1 = nx.get_edge_attributes(G,'weight')
-nx.draw(G,nx.spectral_layout(G,weight = None),with_labels = True)
-nx.draw_networkx_edge_labels(G,nx.spectral_layout(G,weight = None),edge_labels=edge_labels_1)
-
+G,G1,G2,G3 = graphs
 
 graphs = make_edges_universal(graphs)
 
-plt.figure("Test for universal edge weights")
+plt.figure("G")
 edge_labels_1 = nx.get_edge_attributes(G,'weight')
 nx.draw(G,nx.spectral_layout(G,weight = None),with_labels = True)
 nx.draw_networkx_edge_labels(G,nx.spectral_layout(G,weight = None),edge_labels=edge_labels_1)
 
+plt.figure("G1")
+edge_labels_1 = nx.get_edge_attributes(G1,'weight')
+nx.draw(G1,nx.spectral_layout(G1,weight = None),with_labels = True)
+nx.draw_networkx_edge_labels(G1,nx.spectral_layout(G1,weight = None),edge_labels=edge_labels_1)
+
+plt.figure("G2")
+edge_labels_1 = nx.get_edge_attributes(G2,'weight')
+nx.draw(G2,nx.spectral_layout(G2,weight = None),with_labels = True)
+nx.draw_networkx_edge_labels(G2,nx.spectral_layout(G2,weight = None),edge_labels=edge_labels_1)
+
+plt.figure("G3")
+edge_labels_1 = nx.get_edge_attributes(G3,'weight')
+nx.draw(G3,nx.spectral_layout(G3,weight = None),with_labels = True)
+nx.draw_networkx_edge_labels(G3,nx.spectral_layout(G3,weight = None),edge_labels=edge_labels_1)
 
 #A list that stores the time to solve each node.
 time_to_solve = [1]*16
