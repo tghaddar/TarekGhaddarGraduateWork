4ba635368844e2ac9f468da9aa2fa529b1eb68d8
Tarek <tghaddar@tamu.edu>
2019-02-13 17:18:07 -0600

    graph weight inversion works.

--- sweep_optimizer/3d/test_invert_graph.py
+++ sweep_optimizer/3d/test_invert_graph.py
@@ -7,5 +7,30 @@ Created on Wed Feb 13 16:51:31 2019
 """
 
 import networkx as nx
+import matplotlib.pyplot as plt
+from sweep_solver import invert_weights
+from copy import deepcopy
 
-G = nx.Digraph()
+plt.close("all")
+
+G = nx.DiGraph()
+for n in range(0,4):
+  G.add_node(n)
+
+G.add_edge(0,1,weight = 1)
+G.add_edge(1,3,weight = 1/2)
+G.add_edge(0,2,weight = 1)
+G.add_edge(2,3,weight = 1/3)
+
+graph_copy = deepcopy(G)
+
+plt.figure("G")
+edge_labels_1 = nx.get_edge_attributes(G,'weight')
+nx.draw(G,nx.spectral_layout(G),with_labels = True)
+nx.draw_networkx_edge_labels(G,nx.spectral_layout(G),edge_labels=edge_labels_1)
+
+graph_copy = invert_weights(graph_copy)
+plt.figure("G_invert")
+edge_labels_1 = nx.get_edge_attributes(graph_copy,'weight')
+nx.draw(graph_copy,nx.spectral_layout(graph_copy),with_labels = True)
+nx.draw_networkx_edge_labels(graph_copy,nx.spectral_layout(graph_copy),edge_labels=edge_labels_1)
