d33dcc4534af99fa0503920ed321bc8045241cb6
tghaddar <tghaddar92@gmail.com>
2019-04-22 15:24:55 -0500

    Adding data and creating latex tables.

--- MandC2019/FullPaper/generate_metric_tables.py
+++ MandC2019/FullPaper/generate_metric_tables.py
@@ -3,5 +3,8 @@ import numpy as np
 og = np.genfromtxt('opp_side_og_q.csv',delimiter=',')
 lbd = np.genfromtxt('opp_side_lbd_q.csv',delimiter=',')
 
-improvement = np.divide(og,lbd)
+improvement = np.divide(np.subtract(og,lbd),og)
 
+og_tabular = np.savetxt("og_table.csv",og,delimiter=' & ',fmt='%2.3f',newline=' \\\\ \n')
+lbd_tabular = np.savetxt("lbd_table.csv",lbd,delimiter=' & ',fmt='%2.3f',newline=' \\\\ \n')
+improvement_tabular = np.savetxt("improvement_table.csv",improvement,delimiter=' & ',fmt='%2.3f',newline=' \\\\ \n')
