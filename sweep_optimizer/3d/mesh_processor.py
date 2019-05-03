e22cdf6f33d9ff1d1024adcab27f69d366123ce9
Tarek <tghaddar@tamu.edu>
2019-05-03 09:45:46 -0500

    Adding 2d mesh integration.

--- sweep_optimizer/3d/mesh_processor.py
+++ sweep_optimizer/3d/mesh_processor.py
@@ -5,9 +5,12 @@ from scipy import integrate
 
 #Integrating an analytical mesh density function.
 #f = the analytical description of the mesh density function.
-def analytical_mesh_integration(f,xmin,xmax,ymin,ymax,zmin,zmax):
+def analytical_mesh_integration_3d(f,xmin,xmax,ymin,ymax,zmin,zmax):
   
   return integrate.tplquad(f,xmin,xmax,lambda x: ymin, lambda x: ymax, lambda x,y: zmin, lambda x,y: zmax)
+
+def analytical_mesh_integration_2d(f,xmin,xmax,ymin,ymax):
+  return integrate.tplquad(f,xmin,xmax,lambda x: ymin, lambda x: ymax)
   
 
 #Creates uniform 3d cuts given boundaries and number of subsets in each dimension.
