79e16060b56d6a03aaa02248c0b422b2f1c2b70b
Tarek <tghaddar@tamu.edu>
2019-06-10 10:43:07 -0500

    Adding preliminary graph building slides.

--- Defense/python_scripts/plot_adjacency_figures.py
+++ Defense/python_scripts/plot_adjacency_figures.py
@@ -20,3 +20,41 @@ x_cuts,y_cuts = create_2d_cuts(xmin,xmax,numcol,ymin,ymax,numrow)
 bounds = build_global_subset_boundaries(numcol-1,numrow-1,x_cuts,y_cuts)
 plot_subset_boundaries_2d(bounds,num_subsets)
 
+
+#A dictionary for node positions for quadrant 0.
+Q0 = {}
+Q0[-2] = [-2,0]
+Q0[0] = [0,0]
+Q0[1] = [2,1]
+Q0[2] = [2,-1]
+Q0[3] = [4, 0]
+Q0[-1] = [8,0]
+
+#A dictionary for node positions for quadrant 1.
+Q1 = {}
+Q1[-2] = [-2,0]
+Q1[1] = [0,0]
+Q1[0] = [2,-1]
+Q1[3] = [2,1]
+Q1[2] = [4,0]
+Q1[-1] = [8,0]
+
+#A dictionary for node positions for quadrant 2.
+Q2 = {}
+Q2[-2] = [-2,0]
+Q2[2] = [0,0]
+Q2[0] = [2,-1]
+Q2[3] = [2,1]
+Q2[1] = [4,0]
+Q2[-1] = [8,0]
+
+#A dictionary for node positions for quadrant 3.
+Q3 = {}
+Q3[-2] = [-2,0]
+Q3[3] = [0,0]
+Q3[1] = [2,1]
+Q3[2] = [2,-1]
+Q3[0] = [4, 0]
+Q3[-1] = [8,0]
+
+
