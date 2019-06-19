2ab9aa32c9716fc2063f4efd21d3215ba1a7ce90
Tarek <tghaddar@tamu.edu>
2019-06-19 08:23:16 -0500

    Adding slides and changing a figure.

--- Defense/python_scripts/plot_brute_force_optimization.py
+++ Defense/python_scripts/plot_brute_force_optimization.py
@@ -4,7 +4,7 @@ from mpl_toolkits.mplot3d import Axes3D
 plt.close("all")
 
 
-max_times = np.genfromtxt("max_times_0.1.csv")
+max_times = np.genfromtxt("max_times.csv")
 x = max_times[:,0]
 y0 = max_times[:,1]
 y1 = max_times[:,2]
@@ -12,7 +12,10 @@ time = max_times[:,3]
 
 fig = plt.figure("Brute Force Result")
 ax = fig.gca(projection='3d')
-img=ax.scatter(y0,y1,,c=time,cmap=plt.hot())
+img=ax.scatter(x,y0,y1,c=time,cmap=plt.cool())
+ax.set_xlabel("x")
+ax.set_ylabel("y0")
+ax.set_zlabel("y1")
 fig.colorbar(img)
 plt.savefig("../../figures/brute_force_result.pdf")
 plt.close()
