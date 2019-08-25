7c8755be6d1322e93a8aa11223ba4be0134ecfd9
Tarek <tghaddar@tamu.edu>
2019-08-25 12:21:57 -0500

    Adding couple figures.

--- Dissertation/python_scripts/adjacency_portion.py
+++ Dissertation/python_scripts/adjacency_portion.py
@@ -7,6 +7,7 @@ from mesh_processor import create_2d_cuts
 from sweep_solver import plot_subset_boundaries_2d
 from build_global_subset_boundaries import build_global_subset_boundaries
 from build_adjacency_matrix import build_adjacency,build_graphs 
+from flip_adjacency_2d import flip_adjacency
 import networkx as nx
 plt.close("all")
 
@@ -39,43 +40,47 @@ for i in range(0,num_subsets):
 f.write('\\end{pmatrix}$\n')
 f.close()
 
-#Getting the upper triangular portion of the adjacency_matrix
-adjacency_matrix_0 = np.triu(adjacency_matrix)
-f = open("first_adjacency_matrix_ut.txt",'w')
-f.write('$\\begin{pmatrix}\n')
-#Looping over the adjacency matrix and writing it to latex.
-for i in range(0,num_subsets):
-  for j in range(0,num_subsets):
-    f.write(str(int(adjacency_matrix_0[i][j])))
-    if j < num_subsets-1:
-      f.write('&')  
-  f.write('\\\ \n')
-f.write('\\end{pmatrix}$\n')
-f.close()
+##Getting the upper triangular portion of the adjacency_matrix
+#adjacency_matrix_0 = np.triu(adjacency_matrix)
+#f = open("first_adjacency_matrix_ut.txt",'w')
+#f.write('$\\begin{pmatrix}\n')
+##Looping over the adjacency matrix and writing it to latex.
+#for i in range(0,num_subsets):
+#  for j in range(0,num_subsets):
+#    f.write(str(int(adjacency_matrix_0[i][j])))
+#    if j < num_subsets-1:
+#      f.write('&')  
+#  f.write('\\\ \n')
+#f.write('\\end{pmatrix}$\n')
+#f.close()
+#
+##Getting the lower triangular portion of the adjacency_matrix
+#adjacency_matrix_3 = np.tril(adjacency_matrix)
+#f = open("first_adjacency_matrix_lt.txt",'w')
+#f.write('$\\begin{pmatrix}\n')
+##Looping over the adjacency matrix and writing it to latex.
+#for i in range(0,num_subsets):
+#  for j in range(0,num_subsets):
+#    f.write(str(int(adjacency_matrix_3[i][j])))
+#    if j < num_subsets-1:
+#      f.write('&') 
+#  f.write('\\\ \n')
+#f.write('\\end{pmatrix}$\n')
+#f.close()
+#
+##Digraph plot for Q0.
+#plt.figure()
+##Time to build the graph for Q0.
+#G = nx.DiGraph(adjacency_matrix_0)
+#nx.draw(G,nx.kamada_kawai_layout(G),with_labels=True)
+#plt.savefig("../../figures/25_graph0.pdf")
+#
+#
+#plt.figure()
+#G3 = nx.DiGraph(adjacency_matrix_3)
+#nx.draw(G3,nx.kamada_kawai_layout(G3),with_labels=True)
+#plt.savefig("../../figures/25_graph3.pdf")
 
-#Getting the lower triangular portion of the adjacency_matrix
-adjacency_matrix_3 = np.tril(adjacency_matrix)
-f = open("first_adjacency_matrix_lt.txt",'w')
-f.write('$\\begin{pmatrix}\n')
-#Looping over the adjacency matrix and writing it to latex.
-for i in range(0,num_subsets):
-  for j in range(0,num_subsets):
-    f.write(str(int(adjacency_matrix_3[i][j])))
-    if j < num_subsets-1:
-      f.write('&') 
-  f.write('\\\ \n')
-f.write('\\end{pmatrix}$\n')
-f.close()
 
-#Digraph plot for Q0.
-plt.figure()
-#Time to build the graph for Q0.
-G = nx.DiGraph(adjacency_matrix_0)
-nx.draw(G,nx.kamada_kawai_layout(G),with_labels=True)
-plt.savefig("../../figures/25_graph0.pdf")
-
-
-plt.figure()
-G3 = nx.DiGraph(adjacency_matrix_3)
-nx.draw(G3,nx.kamada_kawai_layout(G3),with_labels=True)
-plt.savefig("../../figures/25_graph3.pdf")
+#The flipped adjacency matrix.
+adjacency_flip,id_map = flip_adjacency(adjacency_matrix,num_row,num_col)
