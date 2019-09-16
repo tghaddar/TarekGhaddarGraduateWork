a267caa89bd8cbb008ca0f0136f734acdb192f10
Tarek <tghaddar@tamu.edu>
2019-09-16 12:12:57 -0500

    Almost done with graph primer in TTS chapter.

--- Dissertation/python_scripts/basic_graphs.py
+++ Dissertation/python_scripts/basic_graphs.py
@@ -65,3 +65,20 @@ edge_labels = (nx.get_edge_attributes(G,'weight'))
 nx.draw(G,Q,with_labels=True,arrowsize=20,node_size=2000,font_size=20)
 nx.draw_networkx_edge_labels(G,Q,edge_labels=edge_labels,font_size=14)
 plt.savefig("../../figures/weighted_directed_graph.pdf")
+
+G = nx.DiGraph()
+G.add_nodes_from([0,1,2,3])
+G.add_edge(0,1,weight=-1.0)
+G.add_edge(0,2,weight=-1.0)
+G.add_edge(2,3,weight=-1.0)
+G.add_edge(1,3,weight=-3.0)
+Q = {}
+Q[0] = [0,0]
+Q[1] = [1,1]
+Q[2] = [1,-1]
+Q[3] = [2,0]
+plt.figure()
+edge_labels = (nx.get_edge_attributes(G,'weight'))
+nx.draw(G,Q,with_labels=True,arrowsize=20,node_size=2000,font_size=20)
+nx.draw_networkx_edge_labels(G,Q,edge_labels=edge_labels,font_size=20)
+plt.savefig("../../figures/negative_weighted_directed_graph.pdf")
