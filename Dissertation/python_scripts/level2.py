8bff74ed2e1513211b6748f0950ff2763f69cea0
Tarek <tghaddar@tamu.edu>
2019-09-24 15:49:33 -0500

    Adding binary tree figure to optimizer. Explanation to come.

--- Dissertation/python_scripts/level2.py
+++ Dissertation/python_scripts/level2.py
@@ -69,33 +69,33 @@ x_values = get_highest_jumps(verts[:,0],gxmin,gxmax,numcol)
 x_values,y_cut_suite = create_opt_cut_suite(verts,gxmin,gxmax,gymin,gymax,numcol,numrow)
 
 
-max_times = []
-add_cells = False
-for i in range(0,len(y_cut_suite)):
-  x_cuts = x_values
-  y_cuts = y_cut_suite[i]
-  params = create_parameter_space(x_cuts,y_cuts,numrow,numcol)
-  max_times.append(optimized_tts_numerical(params,points,gxmin,gxmax,gymin,gymax,numrow,numcol,machine_parameters,num_angles,Am,Ay,add_cells,unweighted))
-
-min_index = max_times.index(min(max_times))
-y_cuts_min = y_cut_suite[min_index]
-x_cuts_min = x_values
-
-#Writing the xml portions.
-f = open("level2_opt_cuts.xml",'w')
-f.write("<x_cuts>")
-for x in range(1,numcol):
-  f.write(str(x_cuts_min[x])+" ")
-
-f.write("</x_cuts>\n")
-
-f.write("<y_cuts_by_column>\n")
-for col in range(0,numcol):
-  f.write("  <column>")
-  for y in range(1,numrow):
-    f.write(str(y_cuts_min[col][y])+ " ")
-  
-  f.write("</column>\n")
-
-f.write("</y_cuts_by_column>\n")
-f.close()
+#max_times = []
+#add_cells = False
+#for i in range(0,len(y_cut_suite)):
+#  x_cuts = x_values
+#  y_cuts = y_cut_suite[i]
+#  params = create_parameter_space(x_cuts,y_cuts,numrow,numcol)
+#  max_times.append(optimized_tts_numerical(params,points,gxmin,gxmax,gymin,gymax,numrow,numcol,machine_parameters,num_angles,Am,Ay,add_cells,unweighted))
+#
+#min_index = max_times.index(min(max_times))
+#y_cuts_min = y_cut_suite[min_index]
+#x_cuts_min = x_values
+#
+##Writing the xml portions.
+#f = open("level2_opt_cuts.xml",'w')
+#f.write("<x_cuts>")
+#for x in range(1,numcol):
+#  f.write(str(x_cuts_min[x])+" ")
+#
+#f.write("</x_cuts>\n")
+#
+#f.write("<y_cuts_by_column>\n")
+#for col in range(0,numcol):
+#  f.write("  <column>")
+#  for y in range(1,numrow):
+#    f.write(str(y_cuts_min[col][y])+ " ")
+#  
+#  f.write("</column>\n")
+#
+#f.write("</y_cuts_by_column>\n")
+#f.close()
