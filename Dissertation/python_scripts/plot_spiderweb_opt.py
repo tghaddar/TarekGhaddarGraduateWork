5bf590075d3a2abbc4a6b5d2efdef19cb98a81ef
Tarek <tghaddar@tamu.edu>
2019-10-16 11:40:01 -0500

    Updating figures.

--- Dissertation/python_scripts/plot_spiderweb_opt.py
+++ Dissertation/python_scripts/plot_spiderweb_opt.py
@@ -2,21 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.close("all")
 plt.figure()
-
+plt.grid(True,axis='y')
 
 num_subsets = [2,3,4,5,6,7,8,9,10]
 opt = np.genfromtxt("spiderweb_best_times.csv")
 reg = np.genfromtxt("spiderweb_regular_times.csv")
-plt.plot(num_subsets,opt,'-o',label="Bin")
-plt.plot(num_subsets,reg,'-o',label="Reg")
+plt.plot(num_subsets,opt,'-ro',label="Bin")
+plt.plot(num_subsets,reg,'-bx',label="Reg")
 
 num_subsets = [2,3,4,5,6,7,9,10]
 lb = np.genfromtxt("spiderweb_lb_times.csv")
-plt.plot(num_subsets,lb,'-o',label="LB")
+plt.plot(num_subsets,lb,'-g*',label="LB")
 
 num_subsets = [2,3,4,6,7,9,10]
 lbd = np.genfromtxt("spiderweb_lbd_times.csv")
-plt.plot(num_subsets,lbd,'-o',label="LBD")
+plt.plot(num_subsets,lbd,'-yD',label="LBD")
 plt.legend(loc='best')
 
 plt.ylabel("TTS (s)")
